eeee3741367579ce9d9dfb8eb0af942ce75e4da4
jakubhoma1 <kubahomail@gmail.com>
2026-05-21 17:15:15 +0200

    Ekran rejestracji - prototyp

---
+++ Panel_Glowny/Panel_Glowny/EkranRejestracji.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Panele_Glowne
+{
+    partial class EkranRejestracji
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EkranRejestracji));
+            panel1 = new Panel();
+            button3 = new Button();
+            pictureBox1 = new PictureBox();
+            label5 = new Label();
+            button2 = new Button();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ButtonFace;
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(157, 35);
+            panel1.Margin = new Padding(3, 4, 3, 4);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(517, 650);
+            panel1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ControlLight;
+            button3.FlatAppearance.MouseDownBackColor = SystemColors.GradientInactiveCaption;
+            button3.FlatAppearance.MouseOverBackColor = SystemColors.GradientInactiveCaption;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Location = new Point(90, 488);
+            button3.Margin = new Padding(3, 4, 3, 4);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 47);
+            button3.TabIndex = 11;
+            button3.Text = "Wróć";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(134, 35);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(257, 249);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 10;
+            pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Sitka Small", 14.25F, FontStyle.Bold);
+            label5.Location = new Point(90, 288);
+            label5.Name = "label5";
+            label5.Size = new Size(236, 35);
+            label5.TabIndex = 9;
+            label5.Text = "System Hotelowy";
+            label5.Click += label5_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ControlLight;
+            button2.FlatAppearance.MouseDownBackColor = SystemColors.GradientInactiveCaption;
+            button2.FlatAppearance.MouseOverBackColor = SystemColors.GradientInactiveCaption;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(229, 488);
+            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Name = "button2";
+            button2.Size = new Size(115, 47);
+            button2.TabIndex = 8;
+            button2.Text = "Zarejestruj się";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(149, 433);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
+            textBox3.Name = "textBox3";
+            textBox3.PasswordChar = '*';
+            textBox3.Size = new Size(195, 27);
+            textBox3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label4.Location = new Point(15, 437);
+            label4.Name = "label4";
+            label4.Size = new Size(139, 20);
+            label4.TabIndex = 6;
+            label4.Text = "POWTORZ HASLO:";
+            label4.Click += label4_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(149, 387);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(195, 27);
+            textBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label3.Location = new Point(88, 391);
+            label3.Name = "label3";
+            label3.Size = new Size(62, 20);
+            label3.TabIndex = 4;
+            label3.Text = "HASLO:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label2.Location = new Point(90, 345);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 20);
+            label2.TabIndex = 3;
+            label2.Text = "LOGIN:";
+            label2.Click += label2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(149, 341);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(195, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(57, 588);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ControlLight;
+            button1.FlatAppearance.MouseDownBackColor = SystemColors.GradientInactiveCaption;
+            button1.FlatAppearance.MouseOverBackColor = SystemColors.GradientInactiveCaption;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Arial", 9F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            button1.Location = new Point(373, 579);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(86, 39);
+            button1.TabIndex = 0;
+            button1.Text = "Zamknij";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // EkranRejestracji
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(831, 757);
+            Controls.Add(panel1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "EkranRejestracji";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "EkranRejestracji";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox2;
+        private Label label3;
+        private Button button2;
+        private Label label5;
+        private PictureBox pictureBox1;
+        private Button button3;
+    }
+}
--- Panel_Glowny/Panel_Glowny/obj/Release/net8.0-windows/Panele_Glowne.AssemblyInfo.cs
+++ Panel_Glowny/Panel_Glowny/obj/Release/net8.0-windows/Panele_Glowne.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Panele_Glowne")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdc823c554160dcb6ce50dda8a4dcd95dfa907a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+238a266fb243bde961cc1932f98c318ad3305555")]
 [assembly: System.Reflection.AssemblyProductAttribute("Panele_Glowne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Panele_Glowne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
