b8329aa8daae6cf4e93ae5e2aa77afe789d6d599
januszbyt <janusz.bytnar@pansjar.edu.pl>
2026-05-21 13:40:22 +0200

    dodanie funkcji fun2

--- Panel_Glowny/Panel_Glowny/obj/Debug/net8.0-windows/Panele_Glowne.AssemblyInfo.cs
+++ Panel_Glowny/Panel_Glowny/obj/Debug/net8.0-windows/Panele_Glowne.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Panele_Glowne")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdc823c554160dcb6ce50dda8a4dcd95dfa907a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3ef7b05e756c1817f16ee6d9c21030306af4e9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Panele_Glowne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Panele_Glowne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
