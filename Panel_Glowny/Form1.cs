ff05cd44b36c9991c6b411225cc732a3d7b0b998
Mhomaa <mateuszhomail2@gmail.com>
2026-05-21 10:27:31 +0200

    Panel Glowny

---
+++ Panel_Glowny/Form1.cs
@@ -0,0 +1,19 @@
+namespace Panel_Glowny
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+<<<<<<< HEAD
+        //komentarze testore
+=======
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+>>>>>>> main panel
+    }
+}
