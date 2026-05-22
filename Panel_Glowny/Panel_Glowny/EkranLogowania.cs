60d9d2c13f4b2b10225e9d4d6daec2c7eb4fa9db
Mhomaa <mateuszhomail2@gmail.com>
2026-05-22 12:39:48 +0200

    Polaczenie z baza danych + logowanie wstepne

--- Panel_Glowny/Panel_Glowny/EkranLogowania.cs
+++ Panel_Glowny/Panel_Glowny/EkranLogowania.cs
@@ -1,4 +1,6 @@
-﻿namespace Panele_Glowne;
+﻿using MySql.Data.MySqlClient;
+
+namespace Panele_Glowne;
 
 
 public partial class EkranLogowania : Form
@@ -21,7 +23,52 @@ public partial class EkranLogowania : Form
 
     private void button1_Click(object sender, EventArgs e)
     {
+        string login = textBox1.Text;
+        string haslo = textBox2.Text;
 
+        HotelContext db = new HotelContext();
+
+        using (var conn = db.GetConnection())
+        {
+            try
+            {
+                conn.Open();
+
+                string query = "SELECT Haslo FROM Uzytkownicy WHERE Login = @login LIMIT 1";
+
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@login", login);
+
+                var result = cmd.ExecuteScalar();
+
+                if (result != null)
+                {
+                    string hasloZBazy = result.ToString();
+
+                    if (haslo == hasloZBazy)
+                    {
+                        label5.Text = "Zalogowano poprawnie";
+                        label5.ForeColor = Color.Green;
+
+                    }
+                    else
+                    {
+                        label5.Text = "Błędne hasło";
+                        label5.ForeColor = Color.Red;
+                    }
+                }
+                else
+                {
+                    label5.Text = "Nie ma takiego użytkownika";
+                    label5.ForeColor = Color.Red;
+                }
+            }
+            catch (Exception ex)
+            {
+                label5.Text = "Błąd połączenia z bazą";
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 
 }
---
+++ Panel_Glowny/Panel_Glowny/HotelContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Panele_Glowne
+{
+    internal class HotelContext
+    {
+
+        string server = "127.0.0.1";
+        string port = "3306";
+        string database = "HotelParadise";
+        string user = "HotelP";
+        string password = "PANS!@1234";
+
+        private string connectionString;
+
+        public HotelContext()
+        {
+            connectionString = $"server={server};port={port};database={database};user={user};password={password};";
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
